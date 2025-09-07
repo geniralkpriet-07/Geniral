e32a85d61f2db6063f5d0913dbd32c1cf686f278
Ramjirv32 <ramjib2311@gmail.com>
2025-09-07 17:11:56 +0530

    cors-policyadded

--- backend/config/security.js
+++ backend/config/security.js
@@ -3,7 +3,7 @@ import helmet from "helmet";
 import cors from "cors";
 
 export const corsOptions = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://geniral.vercel.app'],
   credentials: true,
   optionsSuccessStatus: 200
 };
